7b9bd33479a5bf24989c66e225fadb461b7d79a8
M-Yusei1001 <bitterchocolate1001@gmail.com>
2025-06-22 11:43:15 +0900

    wip: ボタンの長押し操作修正

--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e9893c23._.js"
-],
-    source: "dynamic"
-});
